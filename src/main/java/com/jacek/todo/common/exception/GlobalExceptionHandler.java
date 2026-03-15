159fef6321154db4fefcd4995ef8b0b98260cf89
Jacek <laljacek@gmail.com>
2026-03-15 20:38:44 +0100

    add handlers for empty and invalid input

--- src/main/java/com/jacek/todo/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jacek/todo/common/exception/GlobalExceptionHandler.java
@@ -2,10 +2,15 @@ package com.jacek.todo.common.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import tools.jackson.databind.exc.InvalidFormatException;
 
 import java.time.Instant;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -23,5 +28,31 @@ public class GlobalExceptionHandler {
         HttpStatus status = HttpStatus.NOT_FOUND;
         ErrorResponse response = new ErrorResponse(status.value(), e.getMessage(), Instant.now());
         return ResponseEntity.status(status.value()).body(response);
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException e){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        String message = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+
+        ErrorResponse response = new ErrorResponse(status.value(), message, Instant.now());
+        return ResponseEntity.status(status).body(response);
+    }
+
+    @ExceptionHandler(value = HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadable(HttpMessageNotReadableException e){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        Throwable cause = e.getCause();
+        String message = "Invalid JSON format";
+
+        if (cause instanceof InvalidFormatException)
+            message = "Invalid value provided. Please refer to the API documentation";
+
+        ErrorResponse response = new ErrorResponse(status.value(), message, Instant.now());
+        return ResponseEntity.status(status).body(response);
     }
 }

985787095f8f28bfd0b493415ae293baa268a229
Jacek <laljacek@gmail.com>
2026-03-17 01:33:37 +0100

    replace wildcard Lombok imports with explicit imports

--- src/main/java/com/jacek/todo/task/Task.java
+++ src/main/java/com/jacek/todo/task/Task.java
@@ -1,7 +1,9 @@
 package com.jacek.todo.task;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.Instant;
